04295aa21c79030583cdea2bc9c5f42a59545b07
Gilvan Araújo <gilvanjunior@protonmail.com>
2022-04-26 10:30:14 -0300

    format documents

--- spec/helloWorld_spec.lua
+++ spec/helloWorld_spec.lua
@@ -1,9 +1,15 @@
-package.path = '../?.lua;' .. package.path
+package.path = "../?.lua;" .. package.path
 
-local helloWorld = require('helloWorld')
+local helloWorld = require("helloWorld")
 
-describe('simple hello world method', function()
-    it('should return hello world correctly', function()
-        assert.equals(helloWorld.salute(), 'Hello, World!')
-    end)
-end)
+describe(
+    "simple hello world method",
+    function()
+        it(
+            "should return hello world correctly",
+            function()
+                assert.equals(helloWorld.salute(), "Hello, World!")
+            end
+        )
+    end
+)
